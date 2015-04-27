ec763f267018bcaec953bf6ee09e7f4ea1f9a059
BlueSlime <jeremylevilain78@orange.fr>
2015-04-27 17:45:14 +0200

    Fixed NullPointerException

--- src/main/java/net/samagames/hub/gui/stalker/GuiStalker2000.java
+++ src/main/java/net/samagames/hub/gui/stalker/GuiStalker2000.java
@@ -75,7 +75,7 @@ public class GuiStalker2000 extends AbstractGui
             {
                 String formattedServer = ChatColor.GRAY + server.split("_")[0] + " " + server.split("_")[1];
 
-                if(Hub.getInstance().getGameManager().getGameByIdentifier(server.split("_")[0]) == null)
+                if(Hub.getInstance().getGameManager().getGameByIdentifier(server.split("_")[0]) != null)
                     if (Hub.getInstance().getGameManager().getGameByIdentifier(server.split("_")[0]).getCodeName().equals("beta_staff"))
                         formattedServer = ChatColor.RED + "Secret :o";
 
