69cc6b4fae546993c0a04ded45000a7dd433910c
BlueSlime <jeremylevilain78@orange.fr>
2015-07-16 23:30:25 +0200

    Preparation for gikpowair

---
+++ src/main/java/net/samagames/hub/cosmetics/gadgets/displayers/RandomPortalDisplayer.java
@@ -0,0 +1,33 @@
+package net.samagames.hub.cosmetics.gadgets.displayers;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+public class RandomPortalDisplayer extends AbstractDisplayer
+{
+    public RandomPortalDisplayer(Player player)
+    {
+        super(player);
+
+        //MET ICI LA BASE LOCATION PD TA MER
+
+    }
+
+    @Override
+    public void display()
+    {
+
+    }
+
+    @Override
+    public void handleInteraction(Entity who, Entity with)
+    {
+
+    }
+
+    @Override
+    public boolean canUse()
+    {
+        return true;
+    }
+}
