2790773c7e20048134d76440f89864c44e78ec26
Florian Cassayre <florian.cassayre@gmail.com>
2015-09-04 20:21:14 +0200

    Added three particles effects • Rain Effect • Snow Effect • Step Effect

--- src/main/java/net/samagames/hub/cosmetics/particles/ParticleRegistry.java
+++ src/main/java/net/samagames/hub/cosmetics/particles/ParticleRegistry.java
@@ -3,6 +3,10 @@ package net.samagames.hub.cosmetics.particles;
 import net.samagames.hub.cosmetics.common.AbstractCosmeticRegistry;
 import net.samagames.hub.cosmetics.particles.types.FireEffect;
 import net.samagames.hub.cosmetics.particles.types.WitchHatEffect;
+import net.samagames.hub.cosmetics.particles.types.RainyEffect;
+import net.samagames.hub.cosmetics.particles.types.SnowyEffect;
+import net.samagames.hub.cosmetics.particles.types.StepEffect;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
@@ -59,11 +63,32 @@ public class ParticleRegistry extends AbstractCosmeticRegistry<ParticleCosmetic>
         netherParticle.buyableWithStars(0);
 
         ParticleCosmetic fireParticle = new ParticleCosmetic("fire", "Fulminant", new ItemStack(Material.FLINT_AND_STEEL, 1), new String[] {
-                "Vous brûlez d'une râge intense !",
+                "Vous brûlez d'une rage intense !",
                 "",
                 ChatColor.YELLOW + "Particules de feu"
         }, FireEffect.class);
         fireParticle.permissionNeeded("particle.fire");
+        
+        ParticleCosmetic rainParticle = new ParticleCosmetic("rain", "Pluvieux", new ItemStack(Material.SPONGE, 1, (byte) 1), new String[] {
+            "Un nuage s'abat sur vous !",
+            "",
+            ChatColor.YELLOW + "Particules de pluie"
+        }, RainyEffect.class);
+        rainParticle.permissionNeeded("particle.rain");
+        
+        ParticleCosmetic snowParticle = new ParticleCosmetic("snow", "Neigeux", new ItemStack(Material.SNOW_BLOCK, 1), new String[] {
+            "Un nuage s'abat sur vous !",
+            "",
+            ChatColor.YELLOW + "Particules de neige"
+        }, SnowyEffect.class);
+        snowParticle.permissionNeeded("particle.snow");
+        
+        ParticleCosmetic stepParticle = new ParticleCosmetic("step", "Empreintes", new ItemStack(Material.STONE, 1), new String[] {
+            "Montrez votre supériorité !",
+            "",
+            ChatColor.YELLOW + "Particules d'empreintes"
+        }, StepEffect.class);
+        stepParticle.permissionNeeded("particle.step");
 
         /*this.registerElement(heartParticle);
         this.registerElement(waterParticle);
@@ -72,5 +97,8 @@ public class ParticleRegistry extends AbstractCosmeticRegistry<ParticleCosmetic>
         this.registerElement(enchantmentParticle);*/
         this.registerElement(netherParticle);
         this.registerElement(fireParticle);
+        this.registerElement(rainParticle);
+        this.registerElement(snowParticle);
+        this.registerElement(stepParticle);
     }
 }
