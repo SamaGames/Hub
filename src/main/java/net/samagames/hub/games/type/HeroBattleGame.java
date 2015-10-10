b2e29d91f8767752616311142d49ec3bf29824dd
Rigner <rigner@pecher.com>
2015-10-10 14:26:06 +0200

    Fixed colors in shop

--- src/main/java/net/samagames/hub/games/type/HeroBattleGame.java
+++ src/main/java/net/samagames/hub/games/type/HeroBattleGame.java
@@ -1,19 +1,19 @@
 package net.samagames.hub.games.type;
 
+import java.util.Arrays;
+import java.util.List;
+
 import net.samagames.hub.Hub;
 import net.samagames.hub.games.AbstractGame;
 import net.samagames.hub.games.DisplayedStat;
 import net.samagames.hub.games.shop.ShopBuyableCategory;
 import net.samagames.hub.games.shop.ShopCategory;
 import net.samagames.hub.games.shop.ShopImprovableItem;
-import org.bukkit.ChatColor;
+
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.SkullType;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class HeroBattleGame extends AbstractGame
 {
@@ -58,8 +58,8 @@ public class HeroBattleGame extends AbstractGame
     	List<String> classesDisplays = Arrays.asList("Brute", "Guerrier", "Archer", "Mage", "Druide", "Pyrobarbare", "Cryogénie");
     	List<Material> classesItems = Arrays.asList(Material.DIAMOND_CHESTPLATE, Material.DIAMOND_SWORD, Material.BOW, Material.POTION, Material.GOLDEN_APPLE, Material.ICE, Material.FLINT_AND_STEEL);
         List<Integer> prices = Arrays.asList(0, 0, 0, 0, 3000, 5000, 5000);
-    	String cooldownPrefix = ChatColor.GOLD + "Temps réduit de " + ChatColor.AQUA;
-    	String powerPrefix = ChatColor.GOLD + "Niveau " + ChatColor.AQUA;
+    	String cooldownPrefix = "Temps réduit de ";
+    	String powerPrefix = "Niveau ";
         ShopCategory[] categories = new ShopCategory[classes.size()];
         
         int i = 0;
@@ -67,11 +67,11 @@ public class HeroBattleGame extends AbstractGame
         {
         	ShopImprovableItem cooldown = new ShopImprovableItem(this, className + ".cooldown", "Réduction des cooldowns", new ItemStack(Material.WEB), 0, new String[]{"Réduisez le temps de rafraîchissement", "de toutes les capacités pour", "cette classe."});
         	cooldown.addDefault(cooldownPrefix + "0%");
-        	cooldown.addLevel(500, cooldownPrefix + "5%" + ChatColor.GOLD + " à " + ChatColor.AQUA + "10%", "1");
-        	cooldown.addLevel(1000, cooldownPrefix + "10%" + ChatColor.GOLD + " à " + ChatColor.AQUA + "15%", "2");
-        	cooldown.addLevel(5000, cooldownPrefix + "15%" + ChatColor.GOLD + " à " + ChatColor.AQUA + "20%", "3");
-        	cooldown.addLevel(20000, cooldownPrefix + "20%" + ChatColor.GOLD + " à " + ChatColor.AQUA + "25%", "4");
-        	cooldown.addLevel(50000, cooldownPrefix + "25%" + ChatColor.GOLD + " à " + ChatColor.AQUA + "30%", "5");
+        	cooldown.addLevel(500, cooldownPrefix + "5% à 10%", "1");
+        	cooldown.addLevel(1000, cooldownPrefix + "10% à 15%", "2");
+        	cooldown.addLevel(5000, cooldownPrefix + "15% à 20%", "3");
+        	cooldown.addLevel(20000, cooldownPrefix + "20% à 25%", "4");
+        	cooldown.addLevel(50000, cooldownPrefix + "25% à 30%", "5");
         	
         	ShopImprovableItem power = new ShopImprovableItem(this, className + ".power", "Puissance des capacités", new ItemStack(Material.BLAZE_POWDER), 1, new String[]{"Augmentez la puissance et la durée", "de toutes les capacités pour", "cette classe."});
         	power.addDefault(powerPrefix + "0");
