8c8abfba7058768d675b36b79021b5fb2b7a24cf
Silvanosky <geekpower14@gmail.com>
2015-10-11 14:46:18 +0200

    Fix space message

--- src/main/java/net/samagames/hub/games/GameManager.java
+++ src/main/java/net/samagames/hub/games/GameManager.java
@@ -87,7 +87,7 @@ public class GameManager extends AbstractManager
                     {
                         player.sendMessage(ChatColor.GREEN + "Vous avez été ajouté à la queue du jeu " + ChatColor.GOLD + packet.getGame() +  ChatColor.GREEN + " sur la map " + ChatColor.GOLD + packet.getMap() + ChatColor.GREEN + " !");
                     }else{
-                        player.sendMessage(ChatColor.GREEN + "Vous avez été retiré de la queue du jeu" + ChatColor.GOLD + packet.getGame() +  ChatColor.GREEN + " sur la map " + ChatColor.GOLD + packet.getMap() + ChatColor.GREEN + " !");
+                        player.sendMessage(ChatColor.GREEN + "Vous avez été retiré de la queue du jeu " + ChatColor.GOLD + packet.getGame() +  ChatColor.GREEN + " sur la map " + ChatColor.GOLD + packet.getMap() + ChatColor.GREEN + " !");
                     }
                 }
 
