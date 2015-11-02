a5fc759729cf99927dfb70c22814ce0249de8f30
Silvanosky <geekpower14@gmail.com>
2015-11-02 23:46:18 +0100

    Try to fix multiple scoreboard crash

--- src/main/java/net/samagames/hub/common/HubRefresher.java
+++ src/main/java/net/samagames/hub/common/HubRefresher.java
@@ -60,10 +60,7 @@ public class HubRefresher implements Runnable
         {
             e.printStackTrace();
         }
-        finally
-        {
-            jedis.close();
-        }
+        jedis.close();
 
         this.hub.getGuiManager().getPlayersGui().keySet().stream().filter(uuid -> this.hub.getGuiManager().getPlayersGui().get(uuid) instanceof GuiSwitchHub).forEach(uuid -> this.hub.getGuiManager().getPlayersGui().get(uuid).update(Bukkit.getPlayer(uuid)));
     }
