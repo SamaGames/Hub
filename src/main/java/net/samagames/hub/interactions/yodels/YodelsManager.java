d44158c68a4e5606e07a0e7fe8fb52da2c663bbc
frasse_l <frasse_l@epitech.eu>
2016-04-20 23:41:55 +0200

    Try to fix yodels

--- src/main/java/net/samagames/hub/interactions/yodels/YodelsManager.java
+++ src/main/java/net/samagames/hub/interactions/yodels/YodelsManager.java
@@ -6,12 +6,13 @@ import net.samagames.hub.Hub;
 import net.samagames.hub.interactions.AbstractInteractionManager;
 import net.samagames.tools.LocationUtils;
 import org.bukkit.Location;
-
+import org.bukkit.entity.ArmorStand;
 import java.util.logging.Level;
 
 
 public class YodelsManager extends AbstractInteractionManager<Yodel>
 {
+
     public YodelsManager(Hub hub)
     {
         super(hub, "yodels");
@@ -22,26 +23,31 @@ public class YodelsManager extends AbstractInteractionManager<Yodel>
     {
         for (int i = 0; i < rootJson.size(); i++)
         {
-            JsonObject jsonYodel = rootJson.get(i).getAsJsonObject();
-
-            Location startRails = LocationUtils.str2loc(jsonYodel.get("start-rails").getAsString());
-            Location endRails = LocationUtils.str2loc(jsonYodel.get("end-rails").getAsString());
-            Location startPlatform = LocationUtils.str2loc(jsonYodel.get("start-platform").getAsString());
-            Location endPlatform = LocationUtils.str2loc(jsonYodel.get("end-platform").getAsString());
-
-            Yodel yodel = null;
-
             try
             {
-                yodel = new Yodel(this.hub, startRails, endRails, startPlatform, endPlatform);
-            }
-            catch (NullPointerException ignored) {}
+                JsonObject jsonYodel = rootJson.get(i).getAsJsonObject();
 
-            if (yodel != null)
-            {
+                String startRails = jsonYodel.get("start-rails").getAsString();
+                String endRails = jsonYodel.get("end-rails").getAsString();
+
+                Location startPlatform = LocationUtils.str2loc(jsonYodel.get("start-platform").getAsString());
+                Location endPlatform = LocationUtils.str2loc(jsonYodel.get("end-platform").getAsString());
+
+                ArmorStand startArmorStand = (ArmorStand) startPlatform.getWorld().getNearbyEntities(startPlatform, 8.0D, 8.0D, 8.0D).stream().filter(entity -> entity instanceof ArmorStand).filter(entity -> entity.getCustomName().equals(startRails)).findFirst().orElse(null);
+                ArmorStand endArmorStand = (ArmorStand) endPlatform.getWorld().getNearbyEntities(endPlatform, 8.0D, 8.0D, 8.0D).stream().filter(entity -> entity instanceof ArmorStand).filter(entity -> entity.getCustomName().equals(endRails)).findFirst().orElse(null);
+
+                if (startArmorStand == null || endArmorStand == null)
+                    continue ;
+
+                Location startRailsLoc = startArmorStand.getLocation().clone().subtract(0.0D, 1.8D, 0.0D);
+                Location endRailsLoc = endArmorStand.getLocation().clone().subtract(0.0D, 1.8D, 0.0D);
+
+                Yodel yodel = new Yodel(this.hub, startRailsLoc, endRailsLoc, startPlatform, endPlatform);
                 this.interactions.add(yodel);
                 this.log(Level.INFO, "Registered yodel at '" + jsonYodel.get("start-platform").getAsString());
+
             }
+            catch (NullPointerException ignored) {}
         }
     }
 }
