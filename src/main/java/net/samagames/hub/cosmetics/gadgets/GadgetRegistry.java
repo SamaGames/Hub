65b96e1818c6f53497b464161f75f838ae2a9897
frasse_l <frasse_l@epitech.eu>
2016-04-23 14:39:35 +0200

    Moved utils

--- src/main/java/net/samagames/hub/cosmetics/gadgets/GadgetRegistry.java
+++ src/main/java/net/samagames/hub/cosmetics/gadgets/GadgetRegistry.java
@@ -6,6 +6,7 @@ import net.samagames.hub.cosmetics.common.AbstractCosmeticRegistry;
 import net.samagames.hub.cosmetics.common.CosmeticAccessibility;
 import net.samagames.hub.cosmetics.common.CosmeticRarity;
 import net.samagames.hub.cosmetics.gadgets.displayers.*;
+import net.samagames.tools.MojangShitUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.DyeColor;
 import org.bukkit.Material;
