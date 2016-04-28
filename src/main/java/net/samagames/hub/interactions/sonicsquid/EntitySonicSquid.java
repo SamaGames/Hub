b0b0cb1c6048cda314040780b4e3b088492b2375
frasse_l <frasse_l@epitech.eu>
2016-04-28 18:40:46 +0200

    Totally changed EntitySquid, to fix it ! #prayForPoulpy

--- src/main/java/net/samagames/hub/interactions/sonicsquid/EntitySonicSquid.java
+++ src/main/java/net/samagames/hub/interactions/sonicsquid/EntitySonicSquid.java
@@ -5,12 +5,9 @@ import net.minecraft.server.v1_9_R1.*;
 import net.samagames.hub.Hub;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.craftbukkit.v1_9_R1.util.UnsafeList;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.Field;
-import java.util.Iterator;
 import java.util.UUID;
 
 class EntitySonicSquid extends EntitySquid
@@ -82,9 +79,10 @@ class EntitySonicSquid extends EntitySquid
         this.getBukkitEntity().setVelocity(((Player) entityliving.getBukkitEntity()).getEyeLocation().getDirection().multiply(1.025F));**/
 
         this.setYawPitch(entityliving.yaw, 0.0F);
-        this.motX = -Math.sin(entityliving.yaw * Math.PI / 180) * 1.025F;
+        this.motX = -Math.sin(entityliving.yaw * Math.PI / 180) * 0.7F;
+
         this.motY = 0;
-        this.motZ = Math.cos(entityliving.yaw * Math.PI / 180) * 1.025F;
+        this.motZ = Math.cos(entityliving.yaw * Math.PI / 180) * 0.7F;
         this.velocityChanged = true;
         this.move(this.motX, this.motY, this.motZ);
     }
@@ -94,10 +92,13 @@ class EntitySonicSquid extends EntitySquid
     {
         super.move(d0, d1, d2);
 
-        for (int x = -1; x <= 1; ++x)
-            for (int z = -1; z <= 1; ++z)
-                if (this.checkBlock(this.locX + (double) x, this.locY, this.locZ + (double) z))
-                    return ;
+        if (this.checkWater()
+                || (this.locX - (long)locX > 0.7D && this.checkBlock(this.locX + 0.3D, this.locY, this.locZ))
+                || (this.locX - (long)locX < 0.3D && this.checkBlock(this.locX - 0.3D, this.locY, this.locZ))
+                || (this.locZ - (long)locZ > 0.7D && this.checkBlock(this.locZ + 0.3D, this.locY, this.locZ)))
+            return ;
+        if (this.locZ - (long)locZ > 0.7D)
+            this.checkBlock(this.locZ + 0.3D, this.locY, this.locZ);
     }
 
     @Override
@@ -127,19 +128,48 @@ class EntitySonicSquid extends EntitySquid
 
         if (material != net.minecraft.server.v1_9_R1.Material.AIR && material != net.minecraft.server.v1_9_R1.Material.WATER)
         {
-            this.die();
-
-            this.passengers.forEach(passenger ->
-            {
-                passenger.getBukkitEntity().eject();
-                passenger.setLocation(x + 0.5D, y, z + 0.5D, passenger.yaw, passenger.pitch);
-            });
-
+            this.destroySquid(x, y, z);
             return true;
         }
         else
         {
             return false;
+        }
+    }
+
+    private boolean checkWater()
+    {
+        boolean noWater = true;
+        for (int x = -1; x <= 1; x++)
+            for (int y = -1; y <= 1; y++)
+                for (int z = -1; z <= 1; z++)
+                    noWater = noWater && (this.getWorld().getType(new BlockPosition(x, y, z)).getMaterial() != net.minecraft.server.v1_9_R1.Material.WATER);
+        if (noWater)
+            this.destroySquid(this.locX, this.locY, this.locZ);
+        return noWater;
+    }
+
+    private void destroySquid(double x, double y, double z)
+    {
+        this.die();
+
+        this.passengers.forEach(passenger ->
+        {
+            passenger.getBukkitEntity().eject();
+            this.findBetterLandingLocation(passenger, x + 0.5D, y, z + 0.5D, passenger.yaw, passenger.pitch);
+        });
+    }
+
+    private void findBetterLandingLocation(Entity passenger, double x, double y, double z, float yaw, float pitch)
+    {
+        while (y < 256)
+        {
+            if (this.getWorld().getType(new BlockPosition(x, y, z)).getMaterial() == net.minecraft.server.v1_9_R1.Material.AIR)
+            {
+                passenger.setLocation(x, y, z, yaw, pitch);
+                return ;
+            }
+            y++;
         }
     }
 }
