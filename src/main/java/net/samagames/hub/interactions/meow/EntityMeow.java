8d879b6d961cefb62a6ffd6169fe95282098b3da
BlueSlime <jeremylevilain78@orange.fr>
2016-04-30 16:31:42 +0200

    Now taming the cat for him to be friendly

--- src/main/java/net/samagames/hub/interactions/meow/EntityMeow.java
+++ src/main/java/net/samagames/hub/interactions/meow/EntityMeow.java
@@ -2,18 +2,30 @@ package net.samagames.hub.interactions.meow;
 
 import net.minecraft.server.v1_9_R1.*;
 
+import java.util.UUID;
+
 class EntityMeow extends EntityOcelot
 {
+    private static final UUID OWNER_UUID = UUID.fromString("29b2b527-1b59-45df-b7b0-d5ab20d8731a");
+
     EntityMeow(World world)
     {
         super(world);
+
         this.setCatType(2);
     }
 
-    public void setYawPitch(float yaw, float pitch)
+    public void postInit(float yaw, float pitch)
     {
         super.setYawPitch(yaw, pitch);
+
+        this.setTamed(true);
+        this.setOwnerUUID(OWNER_UUID);
+        this.goalSit.setSitting(true);
     }
+
+    @Override
+    protected void da() {}
 
     @Override
     public void b(NBTTagCompound nbttagcompound) {}
