7a8aac25b0cf160b34bdf9c513bf500cdb57a988
BlueSlime <jeremylevilain78@orange.fr>
2016-04-30 16:18:26 +0200

    Removed AI removing (yay)

--- src/main/java/net/samagames/hub/interactions/meow/EntityMeow.java
+++ src/main/java/net/samagames/hub/interactions/meow/EntityMeow.java
@@ -1,32 +1,12 @@
 package net.samagames.hub.interactions.meow;
 
-import com.google.common.collect.Sets;
 import net.minecraft.server.v1_9_R1.*;
-
-import java.lang.reflect.Field;
 
 class EntityMeow extends EntityOcelot
 {
     EntityMeow(World world)
     {
         super(world);
-
-        try
-        {
-            Field bField = PathfinderGoalSelector.class.getDeclaredField("b");
-            bField.setAccessible(true);
-            Field cField = PathfinderGoalSelector.class.getDeclaredField("c");
-            cField.setAccessible(true);
-
-            bField.set(this.goalSelector, Sets.newLinkedHashSet());
-            bField.set(this.targetSelector, Sets.newLinkedHashSet());
-            cField.set(this.goalSelector, Sets.newLinkedHashSet());
-            cField.set(this.targetSelector, Sets.newLinkedHashSet());
-
-            ((Navigation) getNavigation()).a(true);
-        }
-        catch (ReflectiveOperationException ignored) {}
-
         this.setCatType(2);
     }
 
@@ -34,9 +14,6 @@ class EntityMeow extends EntityOcelot
     {
         super.setYawPitch(yaw, pitch);
     }
-
-    @Override
-    protected void r() {}
 
     @Override
     public void b(NBTTagCompound nbttagcompound) {}
