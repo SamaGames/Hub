2fcbff1949a1b46b3f471ecf6ec4a2142750de1d
frasse_l <frasse_l@epitech.eu>
2016-05-12 00:12:06 +0200

    Done Uppervoid shop

--- src/main/java/net/samagames/hub/games/types/UppervoidGame.java
+++ src/main/java/net/samagames/hub/games/types/UppervoidGame.java
@@ -3,6 +3,7 @@ package net.samagames.hub.games.types;
 import net.samagames.hub.Hub;
 import net.samagames.hub.games.AbstractGame;
 import net.samagames.hub.games.shops.ShopCategory;
+import net.samagames.hub.games.shops.ShopDependsItem;
 import net.samagames.hub.games.shops.ShopImprovableItem;
 import net.samagames.tools.RulesBook;
 import org.bukkit.Location;
@@ -76,6 +77,32 @@ public class UppervoidGame extends AbstractGame
     @Override
     public ShopCategory getShopConfiguration()
     {
+        try
+        {
+            ShopCategory shopCategory = new ShopCategory(this.hub, this, 96, 11);
+
+            ShopImprovableItem shooters = new ShopImprovableItem(this.hub, 97, 13, 66);
+            shooters.addLevel(67);
+            shooters.addLevel(68);
+
+            ShopImprovableItem grenades = new ShopImprovableItem(this.hub, 98, 30, 69);
+            for (int i = 70; i <= 74; i++)
+                grenades.addLevel(i);
+
+            ShopImprovableItem hooks = new ShopImprovableItem(this.hub, 99, 32, 75);
+            for (int i = 76; i <= 79; i++)
+                hooks.addLevel(i);
+
+            shopCategory.addContent(shooters);
+            shopCategory.addContent(grenades);
+            shopCategory.addContent(hooks);
+
+            return shopCategory;
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
         return null;
     }
 
