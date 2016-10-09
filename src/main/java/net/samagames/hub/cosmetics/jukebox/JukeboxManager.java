0ea043b63a17ab81117421da885b9411866679f1
BlueSlime <jeremylevilain78@orange.fr>
2016-10-09 15:14:59 +0200

    Fixed achievement exception

--- src/main/java/net/samagames/hub/cosmetics/jukebox/JukeboxManager.java
+++ src/main/java/net/samagames/hub/cosmetics/jukebox/JukeboxManager.java
@@ -245,7 +245,12 @@ public class JukeboxManager extends AbstractCosmeticManager<JukeboxDiskCosmetic>
 
                         this.hub.getServer().getScheduler().runTask(this.hub, () ->
                         {
-                            Arrays.asList(40, 41, 42, 43, 44).forEach(id -> SamaGamesAPI.get().getAchievementManager().incrementAchievement(playerUUID, id, woots));
+                            if (woots > 0)
+                            {
+                                SamaGamesAPI.get().getAchievementManager().getAchievementByID(40).unlock(playerUUID);
+                                Arrays.asList(41, 42, 43, 44).forEach(id -> SamaGamesAPI.get().getAchievementManager().incrementAchievement(playerUUID, id, woots));
+                            }
+
                             Arrays.asList(55, 56, 57).forEach(id -> SamaGamesAPI.get().getAchievementManager().incrementAchievement(playerUUID, id, this.currentPlaylist.getDisk().getSeconds() / 60));
                         });
                     }
