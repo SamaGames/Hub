c69b622acff9d37807232d82dba0a9a9a4d736b7
BlueSlime <jeremylevilain78@orange.fr>
2016-10-14 18:35:30 +0200

    Continuing animation

--- src/main/java/net/samagames/hub/interactions/graou/OpeningAnimationRunnable.java
+++ src/main/java/net/samagames/hub/interactions/graou/OpeningAnimationRunnable.java
@@ -3,21 +3,39 @@ package net.samagames.hub.interactions.graou;
 import net.minecraft.server.v1_10_R1.PathEntity;
 import net.samagames.hub.Hub;
 import net.samagames.hub.utils.ProximityUtils;
+import net.samagames.tools.BlockUtils;
+import net.samagames.tools.ItemUtils;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.SkullType;
 import org.bukkit.Sound;
+import org.bukkit.block.Skull;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Optional;
+import java.util.Random;
 
 class OpeningAnimationRunnable implements Runnable
 {
+    private static final String[] PRESENT_TEXTURES = new String[] {
+            "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZWUzYThmZDA4NTI5Nzc0NDRkOWZkNzc5N2NhYzA3YjhkMzk0OGFkZGM0M2YwYmI1Y2UyNWFlNzJkOTVkYyJ9fX0=", // Orange
+            "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjczYTIxMTQxMzZiOGVlNDkyNmNhYTUxNzg1NDE0MDM2YTJiNzZlNGYxNjY4Y2I4OWQ5OTcxNmM0MjEifX19", // Red
+            "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNzg1MzZhNDYxNjg0ZmM3YTYzYjU0M2M1ZGYyMzQ4Y2Q5NjhiZjU1ODM1OTFiMWJiY2M1ZjBkYjgzMTY2ZGM3In19fQ==", // Grey blue
+            "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDNlMTY1ODU3YzRjMmI3ZDZkOGQ3MGMxMDhjYzZkNDY0ZjdmMzBlMDRkOTE0NzMxYjU5ZTMxZDQyNWEyMSJ9fX0=", // Dark blue
+            "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvODRlMWM0MmYxMTM4M2I5ZGM4ZTY3ZjI4NDZmYTMxMWIxNjMyMGYyYzJlYzdlMTc1NTM4ZGJmZjFkZDk0YmI3In19fQ==", // Light blue
+            "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYWMzODIxZDRmNjFiMTdmODJmMGQ3YThlNTMxMjYwOGZmNTBlZGUyOWIxYjRkYzg5ODQ3YmU5NDI3ZDM2In19fQ==" // Grey
+    };
+
     private final Hub hub;
     private final Graou graou;
     private final Player player;
     private final Location[] treasureLocations;
     private final Location[] openingLocations;
+    private Item present;
 
     OpeningAnimationRunnable(Hub hub, Graou graou, Player player, Location[] treasureLocations, Location[] openingLocations)
     {
@@ -47,6 +65,7 @@ class OpeningAnimationRunnable implements Runnable
             public void run()
             {
                 Optional<Entity> entity = ProximityUtils.getNearbyEntities(OpeningAnimationRunnable.this.treasureLocations[0], 3.0D).stream()
+                        .filter(e -> e.getUniqueId() != null)
                         .filter(e -> e.getUniqueId() == OpeningAnimationRunnable.this.graou.getGraouEntity().getUniqueID())
                         .findAny();
 
@@ -66,6 +85,14 @@ class OpeningAnimationRunnable implements Runnable
         this.hub.getServer().getScheduler().runTaskLater(this.hub, () ->
         {
             this.treasureLocations[0].getWorld().playSound(this.treasureLocations[0], Sound.BLOCK_CHEST_CLOSE, 1.0F, 1.0F);
+
+            final String selectedPresentTexture = PRESENT_TEXTURES[new Random().nextInt(PRESENT_TEXTURES.length)];
+
+            this.present = this.treasureLocations[0].getWorld().spawn(this.treasureLocations[0], Item.class);
+            this.present.setItemStack(ItemUtils.getCustomHead(selectedPresentTexture));
+
+            this.graou.getGraouEntity().getBukkitEntity().setPassenger(this.present);
+
             this.walk(this.openingLocations[1]);
 
             new BukkitRunnable()
@@ -74,12 +101,14 @@ class OpeningAnimationRunnable implements Runnable
                 public void run()
                 {
                     Optional<Entity> entity = ProximityUtils.getNearbyEntities(OpeningAnimationRunnable.this.openingLocations[0], 2.0D).stream()
+                            .filter(e -> e.getUniqueId() != null)
                             .filter(e -> e.getUniqueId() == OpeningAnimationRunnable.this.graou.getGraouEntity().getUniqueID())
                             .findAny();
 
                     if (entity.isPresent())
                     {
-                        OpeningAnimationRunnable.this.finish();
+                        BlockUtils.setCustomSkull(OpeningAnimationRunnable.this.openingLocations[0].getBlock(), selectedPresentTexture);
+
                         this.cancel();
                     }
                 }
