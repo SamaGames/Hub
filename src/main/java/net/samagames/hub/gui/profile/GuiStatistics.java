ddcffb381605a44d2cb1936e0811ab1e9d990db8
BlueSlime <jeremylevilain78@orange.fr>
2016-12-26 18:19:02 +0100

    Added missing statistics. Job finished.

--- src/main/java/net/samagames/hub/gui/profile/GuiStatistics.java
+++ src/main/java/net/samagames/hub/gui/profile/GuiStatistics.java
@@ -59,13 +59,74 @@ class GuiStatistics extends AbstractGui
                 Pair.of("Mehs reçus", playerStats.getJukeBoxStatistics()::getMehs)
         ));
 
-        this.setGameStatisticsSlotData("UHC", this.hub.getGameManager().getGameByIdentifier("uhczone").getIcon(), Arrays.asList(
-                Pair.of("Woots reçus", playerStats.getJukeBoxStatistics()::getWoots),
-                Pair.of("Woots donnés", playerStats.getJukeBoxStatistics()::getWootsGiven),
-                Pair.of("Mehs reçus", playerStats.getJukeBoxStatistics()::getMehs)
+        this.setGameStatisticsSlotData("UHC", new ItemStack(Material.GOLDEN_APPLE, 1), Arrays.asList(
+                Pair.of("Parties jouées", playerStats.getUHCOriginalStatistics()::getPlayedGames),
+                Pair.of("Parties gagnées", playerStats.getUHCOriginalStatistics()::getWins),
+                Pair.of("Meurtres", playerStats.getUHCOriginalStatistics()::getKills),
+                Pair.of("Morts", playerStats.getUHCOriginalStatistics()::getDeaths)
         ));
 
-        this.setSlotData(ChatColor.YELLOW + "Voir votre profil en ligne", new ItemStack(Material.NETHER_STAR, 1), this.inventory.getSize() - 4, null, "website");
+        this.setGameStatisticsSlotData("UHCRun", new ItemStack(Material.GOLDEN_APPLE, 1), Arrays.asList(
+                Pair.of("Parties jouées", playerStats.getUHCRunStatistics()::getPlayedGames),
+                Pair.of("Parties gagnées", playerStats.getUHCRunStatistics()::getWins),
+                Pair.of("Meurtres", playerStats.getUHCRunStatistics()::getKills),
+                Pair.of("Morts", playerStats.getUHCRunStatistics()::getDeaths)
+        ));
+
+        this.setGameStatisticsSlotData("DoubleRunner", new ItemStack(Material.GOLDEN_APPLE, 1), Arrays.asList(
+                Pair.of("Parties jouées", playerStats.getDoubleRunnerStatistics()::getPlayedGames),
+                Pair.of("Parties gagnées", playerStats.getDoubleRunnerStatistics()::getWins),
+                Pair.of("Meurtres", playerStats.getDoubleRunnerStatistics()::getKills),
+                Pair.of("Morts", playerStats.getDoubleRunnerStatistics()::getDeaths)
+        ));
+
+        this.setGameStatisticsSlotData("UHCRandom", new ItemStack(Material.GOLDEN_APPLE, 1), Arrays.asList(
+                Pair.of("Parties jouées", playerStats.getUHCRandomStatistics()::getPlayedGames),
+                Pair.of("Parties gagnées", playerStats.getUHCRandomStatistics()::getWins),
+                Pair.of("Meurtres", playerStats.getUHCRandomStatistics()::getKills),
+                Pair.of("Morts", playerStats.getUHCRandomStatistics()::getDeaths)
+        ));
+
+        this.setGameStatisticsSlotData("RandomRun", new ItemStack(Material.GOLDEN_APPLE, 1), Arrays.asList(
+                Pair.of("Parties jouées", playerStats.getRandomRunStatistics()::getPlayedGames),
+                Pair.of("Parties gagnées", playerStats.getRandomRunStatistics()::getWins),
+                Pair.of("Meurtres", playerStats.getRandomRunStatistics()::getKills),
+                Pair.of("Morts", playerStats.getRandomRunStatistics()::getDeaths)
+        ));
+
+        this.setGameStatisticsSlotData("Run4Flag", new ItemStack(Material.GOLDEN_APPLE, 1), Arrays.asList(
+                Pair.of("Parties jouées", playerStats.getUltraFlagKeeperStatistics()::getPlayedGames),
+                Pair.of("Parties gagnées", playerStats.getUltraFlagKeeperStatistics()::getWins),
+                Pair.of("Meurtres", playerStats.getUltraFlagKeeperStatistics()::getKills),
+                Pair.of("Morts", playerStats.getUltraFlagKeeperStatistics()::getDeaths),
+                Pair.of("Drapeaux capturés", playerStats.getUltraFlagKeeperStatistics()::getFlagsCaptured),
+                Pair.of("Drapeaux retournés", playerStats.getUltraFlagKeeperStatistics()::getFlagsReturned)
+        ));
+
+        this.setGameStatisticsSlotData("Quake", new ItemStack(Material.DIAMOND_HOE, 1), Arrays.asList(
+                Pair.of("Parties jouées", playerStats.getQuakeStatistics()::getPlayedGames),
+                Pair.of("Parties gagnées", playerStats.getQuakeStatistics()::getWins),
+                Pair.of("Meurtres", playerStats.getQuakeStatistics()::getKills),
+                Pair.of("Morts", playerStats.getQuakeStatistics()::getDeaths)
+        ));
+
+        this.setGameStatisticsSlotData("Uppervoid", new ItemStack(Material.STICK, 1), Arrays.asList(
+                Pair.of("Parties jouées", playerStats.getUppervoidStatistics()::getPlayedGames),
+                Pair.of("Parties gagnées", playerStats.getUppervoidStatistics()::getWins),
+                Pair.of("Meurtres", playerStats.getUppervoidStatistics()::getKills),
+                Pair.of("TNT lancées", playerStats.getUppervoidStatistics()::getTntLaunched),
+                Pair.of("Grenades lancées", playerStats.getUppervoidStatistics()::getGrenades),
+                Pair.of("Blocs cassés", playerStats.getUppervoidStatistics()::getBlocks)
+        ));
+
+        this.setGameStatisticsSlotData("Dimensions", new ItemStack(Material.EYE_OF_ENDER, 1), Arrays.asList(
+                Pair.of("Parties jouées", playerStats.getDimensionStatistics()::getPlayedGames),
+                Pair.of("Parties gagnées", playerStats.getDimensionStatistics()::getWins),
+                Pair.of("Meurtres", playerStats.getDimensionStatistics()::getKills),
+                Pair.of("Morts", playerStats.getDimensionStatistics()::getDeaths)
+        ));
+
+        this.setSlotData(ChatColor.YELLOW + "Voir votre profil en ligne", new ItemStack(Material.NETHER_STAR, 1), this.inventory.getSize() - 6, null, "website");
         this.setSlotData(getBackIcon(), this.inventory.getSize() - 4, "back");
     }
 
