6218682ae3aa3a0e0b6fc9157dc722ea83cc678e
BlueSlime <jeremylevilain78@orange.fr>
2017-02-17 23:35:20 +0100

    Using now the good table to store event data

--- src/main/java/net/samagames/hub/commands/animating/CommandEvent.java
+++ src/main/java/net/samagames/hub/commands/animating/CommandEvent.java
@@ -6,6 +6,8 @@ import net.samagames.hub.Hub;
 import net.samagames.hub.commands.AbstractCommand;
 import net.samagames.hub.games.AbstractGame;
 import net.samagames.persistanceapi.GameServiceManager;
+import net.samagames.persistanceapi.beans.events.EventBean;
+import net.samagames.persistanceapi.beans.events.EventWinnerBean;
 import net.samagames.persistanceapi.beans.players.SanctionBean;
 import net.samagames.tools.chat.ChatUtils;
 import net.samagames.tools.chat.fanciful.FancyMessage;
@@ -15,6 +17,7 @@ import org.bukkit.entity.Player;
 import redis.clients.jedis.Jedis;
 
 import java.lang.reflect.Field;
+import java.sql.Timestamp;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
@@ -27,7 +30,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class CommandEvent extends AbstractCommand
 {
-    private static final int[][] PRICES = new int[][] {{ 50, 0, 60 }, { 200, 0, 60*20 }, { 0, 1, 60*60 }, { 500, 1, 60*60*12 }, { 2000, 3, 60*60*24*5 }};
+    private static final int[][] REWARDS = new int[][] {{ 50, 0, 60 }, { 200, 0, 60*20 }, { 0, 1, 60*60 }, { 500, 1, 60*60*12 }, { 2000, 3, 60*60*24*5 }};
 
     public CommandEvent(Hub hub)
     {
@@ -45,8 +48,8 @@ public class CommandEvent extends AbstractCommand
 
             Jedis jedis = SamaGamesAPI.get().getBungeeResource();
 
-            for (int i = 0; i < PRICES.length; i++)
-                this.showPrices(jedis, player, null, null, i, PRICES[i][0], PRICES[i][1]);
+            for (int i = 0; i < REWARDS.length; i++)
+                this.showPrices(jedis, player, null, null, i, REWARDS[i][0], REWARDS[i][1]);
 
             jedis.close();
 
@@ -77,11 +80,11 @@ public class CommandEvent extends AbstractCommand
             {
                 String gameCodeName = args[1];
                 String map = args[2];
-                int pricesId = Integer.parseInt(args[3]);
+                int rewardsId = Integer.parseInt(args[3]);
 
                 jedis = SamaGamesAPI.get().getBungeeResource();
 
-                if (jedis.exists("hub:event:cooldown:" + player.getUniqueId().toString() + ":" + pricesId))
+                if (jedis.exists("hub:event:cooldown:" + player.getUniqueId().toString() + ":" + rewardsId))
                 {
                     player.sendMessage("Impossible de créer l'événement : les gains choisis sont indisponibles pour le moment.");
                     jedis.close();
@@ -92,24 +95,39 @@ public class CommandEvent extends AbstractCommand
                 String template = gameCodeName.equals("hub") ? SamaGamesAPI.get().getServerName().split("_")[1] : this.hub.getGameManager().getGameByIdentifier(gameCodeName).getGameSignsByMap(map).get(0).getTemplate();
 
                 Calendar calendar = Calendar.getInstance();
-                calendar.add(Calendar.SECOND, PRICES[pricesId][2]);
+                calendar.add(Calendar.SECOND, REWARDS[rewardsId][2]);
 
-                jedis.set("hub:event:current:" + player.getUniqueId().toString(), gameCodeName + ":" + template + ":" + String.valueOf(pricesId));
-                jedis.set("hub:event:cooldown:" + player.getUniqueId().toString() + ":" + pricesId, String.valueOf(calendar.getTime().getTime()));
-                jedis.expire("hub:event:cooldown:" + player.getUniqueId().toString() + ":" + pricesId, PRICES[pricesId][2]);
-
-                jedis.close();
+                int eventId = -1;
 
                 try
                 {
                     Class<?> coreAPIPluginClass = Class.forName("net.samagames.core.APIPlugin");
                     Field gameServiceManagerField = coreAPIPluginClass.getDeclaredField("gameServiceManager");
                     gameServiceManagerField.setAccessible(true);
-                    GameServiceManager gameServiceManager = (GameServiceManager) gameServiceManagerField.get(this.hub.getServer().getPluginManager().getPlugin("SamaGamesAPI"));
 
-                    gameServiceManager.applySanction(SanctionBean.TEXT, new SanctionBean(player.getUniqueId(), SanctionBean.TEXT, "Evénement créé : " + gameCodeName + ", " + map + ", " + PRICES[pricesId][0] + " pièces et " + PRICES[pricesId][1] + " perles.", null, false));
+                    Timestamp now = new Timestamp(new Date().getTime());
+
+                    GameServiceManager gameServiceManager = (GameServiceManager) gameServiceManagerField.get(this.hub.getServer().getPluginManager().getPlugin("SamaGamesAPI"));
+                    gameServiceManager.createEvent(new EventBean(0, player.getUniqueId(), template, REWARDS[rewardsId][0], REWARDS[rewardsId][1], now));
+
+                    eventId = gameServiceManager.getEvent(now).getEventId();
                 }
-                catch (Exception ignored) {}
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+                if (eventId == -1)
+                {
+                    player.sendMessage(ChatColor.RED + "Une erreur s'est produite, veuillez contacter un développeur.");
+                    return true;
+                }
+
+                jedis.set("hub:event:current:" + player.getUniqueId().toString(), eventId + ":" + gameCodeName + ":" + template + ":" + String.valueOf(rewardsId));
+                jedis.set("hub:event:cooldown:" + player.getUniqueId().toString() + ":" + rewardsId, String.valueOf(calendar.getTime().getTime()));
+                jedis.expire("hub:event:cooldown:" + player.getUniqueId().toString() + ":" + rewardsId, REWARDS[rewardsId][2]);
+
+                jedis.close();
 
                 player.sendMessage(ChatColor.GREEN + "Votre gain est verouillé. La commande " + ChatColor.GOLD + "/event win <pseudo>" + ChatColor.GREEN + " donnera au joueur les gains sélectionnés.");
 
@@ -122,13 +140,13 @@ public class CommandEvent extends AbstractCommand
                     player.sendMessage(ChatColor.GREEN + "Votre serveur à été commandé. Veut sera téléporté dessus en tant que modérateur. Vous ne pourrez donc pas participer au jeu.");
                 }
 
-                this.hub.getServer().getScheduler().runTaskLaterAsynchronously(this.hub, () -> SamaGamesAPI.get().getPubSub().send("eventChannel", gameCodeName + ":" + template + ":" + PRICES[pricesId][0] + ":" + PRICES[pricesId][1]), 20L);
+                this.hub.getServer().getScheduler().runTaskLaterAsynchronously(this.hub, () -> SamaGamesAPI.get().getPubSub().send("eventChannel", gameCodeName + ":" + template + ":" + REWARDS[rewardsId][0] + ":" + REWARDS[rewardsId][1]), 20L);
             }
             else if (args.length == 4)
             {
                 String gameCodeName = args[1];
                 String map = args[2];
-                int pricesId = Integer.parseInt(args[3]);
+                int rewardsId = Integer.parseInt(args[3]);
 
                 player.sendMessage(ChatUtils.getCenteredText(ChatColor.WHITE + "• Étape 4 : Confirmation •"));
                 player.sendMessage("");
@@ -136,7 +154,7 @@ public class CommandEvent extends AbstractCommand
                 player.sendMessage(ChatColor.WHITE + "- Jeu : " + ChatColor.GRAY + (gameCodeName.equals("hub") ? "Hub" : this.hub.getGameManager().getGameByIdentifier(gameCodeName).getName()));
                 player.sendMessage(ChatColor.WHITE + "- Carte : " + ChatColor.GRAY + map);
 
-                int[] prices = PRICES[pricesId];
+                int[] prices = REWARDS[rewardsId];
                 String pricesLine = "";
 
                 if (prices[0] > 0)
@@ -151,7 +169,7 @@ public class CommandEvent extends AbstractCommand
                 player.sendMessage(ChatColor.WHITE + "- Gains : " + pricesLine);
                 player.sendMessage("");
 
-                new FancyMessage("[Lancer l'événement]").color(ChatColor.GREEN).command("/event create " + gameCodeName + " " + map + " " + pricesId + " confirm").send(player);
+                new FancyMessage("[Lancer l'événement]").color(ChatColor.GREEN).command("/event create " + gameCodeName + " " + map + " " + rewardsId + " confirm").send(player);
             }
             else if (args.length == 3)
             {
@@ -163,8 +181,8 @@ public class CommandEvent extends AbstractCommand
 
                 jedis = SamaGamesAPI.get().getBungeeResource();
 
-                for (int i = 0; i < PRICES.length; i++)
-                    this.showPrices(jedis, player, gameCodeName, template, i, PRICES[i][0], PRICES[i][1]);
+                for (int i = 0; i < REWARDS.length; i++)
+                    this.showPrices(jedis, player, gameCodeName, template, i, REWARDS[i][0], REWARDS[i][1]);
 
                 jedis.close();
             }
@@ -249,21 +267,38 @@ public class CommandEvent extends AbstractCommand
                 return true;
             }
 
-            int pricesId = Integer.parseInt(jedis.get("hub:event:current:" + player.getUniqueId().toString()).split(":")[2]);
+            String[] eventData = jedis.get("hub:event:current:" + player.getUniqueId().toString()).split(":");
 
-            if (PRICES[pricesId][0] > 0)
-                SamaGamesAPI.get().getPlayerManager().getPlayerData(this.hub.getServer().getPlayer(playerName).getUniqueId()).creditCoins(PRICES[pricesId][0], "Evénement", false);
+            int eventId = Integer.parseInt(eventData[0]);
+            int rewardsId = Integer.parseInt(eventData[3]);
 
-            if (PRICES[pricesId][1] > 0)
+            if (REWARDS[rewardsId][0] > 0)
+                SamaGamesAPI.get().getPlayerManager().getPlayerData(this.hub.getServer().getPlayer(playerName).getUniqueId()).creditCoins(REWARDS[rewardsId][0], "Evénement", false);
+
+            if (REWARDS[rewardsId][1] > 0)
             {
                 Calendar calendar = Calendar.getInstance();
                 calendar.add(Calendar.DAY_OF_MONTH, 7);
 
-                for (int i = 0; i < PRICES[pricesId][1]; i++)
+                for (int i = 0; i < REWARDS[rewardsId][1]; i++)
                     this.hub.getInteractionManager().getWellManager().addPearlToPlayer(this.hub.getServer().getPlayer(playerName), new Pearl(UUID.randomUUID(), new Random().nextInt(4) + 1, calendar.getTime().getTime()));
 
                 this.hub.getInteractionManager().getGraouManager().update(this.hub.getServer().getPlayer(playerName));
-                this.hub.getServer().getPlayer(playerName).sendMessage(ChatColor.GREEN + "+" + PRICES[pricesId][1] + " perle" + (PRICES[pricesId][1] > 1 ? "s" : "") + " (Evénement)");
+                this.hub.getServer().getPlayer(playerName).sendMessage(ChatColor.GREEN + "+" + REWARDS[rewardsId][1] + " perle" + (REWARDS[rewardsId][1] > 1 ? "s" : "") + " (Evénement)");
+            }
+
+            try
+            {
+                Class<?> coreAPIPluginClass = Class.forName("net.samagames.core.APIPlugin");
+                Field gameServiceManagerField = coreAPIPluginClass.getDeclaredField("gameServiceManager");
+                gameServiceManagerField.setAccessible(true);
+
+                GameServiceManager gameServiceManager = (GameServiceManager) gameServiceManagerField.get(this.hub.getServer().getPluginManager().getPlugin("SamaGamesAPI"));
+                gameServiceManager.createEventWinner(new EventWinnerBean(0, eventId, this.hub.getServer().getPlayer(playerName).getUniqueId()));
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
             }
 
             this.hub.getScoreboardManager().update(this.hub.getServer().getPlayer(playerName));
@@ -289,9 +324,10 @@ public class CommandEvent extends AbstractCommand
 
             jedis.close();
 
-            String gameCodeName = eventData[0];
-            String map = eventData[1];
-            int pricesId = Integer.parseInt(eventData[2]);
+            int eventId = Integer.parseInt(eventData[0]);
+            String gameCodeName = eventData[1];
+            String map = eventData[2];
+            int rewardsId = Integer.parseInt(eventData[3]);
 
             if (!gameCodeName.equals("hub"))
             {
@@ -299,7 +335,7 @@ public class CommandEvent extends AbstractCommand
                 return true;
             }
 
-            SamaGamesAPI.get().getPubSub().send("eventChannel", gameCodeName + ":" + map + ":" + PRICES[pricesId][0] + ":" + PRICES[pricesId][1]);
+            SamaGamesAPI.get().getPubSub().send("eventChannel", gameCodeName + ":" + map + ":" + REWARDS[rewardsId][0] + ":" + REWARDS[rewardsId][1]);
         }
         else if (subCommand.equals("end"))
         {
@@ -369,6 +405,7 @@ public class CommandEvent extends AbstractCommand
         this.showHelpSubCommand(player, "create", "Créer un événement pas à pas");
         this.showHelpSubCommand(player, "win <pseudo>", "Déclarer gagnant un joueur et lui donner sa récompense");
         this.showHelpSubCommand(player, "remind", "Refaire l'annonce d'un événement en cours");
+        this.showHelpSubCommand(player, "end", "Terminer l'événement en cours. A utiliser en cas de grande nécéssité");
 
         player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
     }
